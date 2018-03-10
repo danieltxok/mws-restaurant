d45c6c3ecdd535110853101ce0379c265b91c3c3
Daniel Barcos <dbarcosb@gmail.com>
2018-03-10 11:14:02 +0100

    feat: Added proper offline behaviour to SW

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,6 @@
 // defines what I want to cache when installing SW
-const staticCacheName = 'rw-static-v2';
+const staticCache = 'rw-static-v2';
+const responsesCache = 'rw-responses';
 const urlsToStaticCache = [
     '/',
     '/index.html',
@@ -21,13 +22,18 @@ const urlsToStaticCache = [
     'img/10.jpg'
 ];
 const contentImgsCache = 'rw-imgs-v1';
-const allCaches = [staticCacheName, contentImgsCache];
+const allCaches = [staticCache, contentImgsCache, responsesCache]; //here we keep the important caches we need
 
 // install event
 self.addEventListener('install', event => {
-    console.log('The service worker is being installed');
+
+    // e.waitUntil delays the event until the promise resolves
     event.waitUntil(
-        caches.open(staticCacheName).then(cache => {
+
+        // Open the cache
+        caches.open(staticCache).then(cache => {
+
+            // addAll the files to the cache
             return cache.addAll(urlsToStaticCache);
             // addAll is atomic and uses fetch under the hood, so the request will always go via the browser cache
         })
@@ -36,9 +42,15 @@ self.addEventListener('install', event => {
 
 // activate event
 self.addEventListener('activate', event => {
+    console.log('SW activation');
+
     event.waitUntil(
+
+        // get all the cache keys
         caches.keys().then(cacheNames => {
             return Promise.all(
+
+                // filter all caches to find caches starting with rw- that are not inside allCaches and delete them
                 cacheNames.filter(cacheName => {
                     return cacheName.startsWith('rw-') && !allCaches.includes(cacheName);
                 }).map(cacheName => {
@@ -51,37 +63,63 @@ self.addEventListener('activate', event => {
 
 // fetch event
 self.addEventListener('fetch', event => {
-    const requestUrl = new URL(event.request.url);
-    // if (requestUrl.origin === location.origin) {
-        // console.log(requestUrl);
-        if (requestUrl.pathname.startsWith('/img/')) {
-            event.respondWith(servePhoto(event.request));
-            return;
-        }
-        event.respondWith(
-            caches.match(event.request).then(response => {
-                return response || fetch(event.request);
-            })
-        );
-    // }
-    
-    // if (event.request.url.startsWith('http://localhost:3000/img/')) {
-    //     event.respondWith(
-    //         fetch('img/icon.svg')
-    //     );
-    // }
+    // const requestUrl = new URL(e.request.url);
+    // console.log('SW Fetch', requestUrl);
+
+    event.respondWith(
+
+        // Check in cache for the request
+        caches.match(event.request).then(response => {
+
+            // If response in cache, return cached version
+            if (response) {
+                console.log('Served from cache:', event.request.url, response);
+                return response;
+            }
+
+            // If response not in cache, fetch & cache
+            fetch(event.request).then(response => {
+
+                // Open the cache
+                caches.open(responsesCache).then(cache => {
+
+                    // Put the cloned response in the cache
+                    cache.put(event.request, response.clone());
+                    console.log('Response cached:', event.request.url);
+
+                    return response;
+                }); 
+            });
+
+        })
+    );
 });
 
-function servePhoto(request) {
-    const storageUrl = request.url.split('_')[0];
-    return caches.open(contentImgsCache).then(cache => {
-        return cache.match(storageUrl).then(response => {
-            if (response) return response;
 
-            return fetch(request).then(networkResponse => {
-                cache.put(storageUrl, networkResponse.clone());
-                return networkResponse;
-            });
-        });
-    });
-}
+// const requestUrl = new URL(e.request.url);
+// console.log(requestUrl.indexOf('https://maps.googleapi.com/js'));
+// if (requestUrl.origin === location.origin) {
+// console.log(requestUrl);
+// if (requestUrl.pathname.startsWith('/img/')) {
+//     e.respondWith(servePhoto(e.request));
+//     return;
+// }
+// e.respondWith(
+//     caches.match(e.request).then(response => {
+//         return response || fetch(e.request);
+//     })
+// );
+// }
+// function servePhoto(request) {
+//     const storageUrl = request.url.split('_')[0];
+//     return caches.open(contentImgsCache).then(cache => {
+//         return cache.match(storageUrl).then(response => {
+//             if (response) return response;
+
+//             return fetch(request).then(networkResponse => {
+//                 cache.put(storageUrl, networkResponse.clone());
+//                 return networkResponse;
+//             });
+//         });
+//     });
+// }
--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('watch', function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/**/*.js', browserSync.reload);
     // Other watchers
 })
 
