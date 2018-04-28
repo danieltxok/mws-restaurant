30cb1059d46e0d8fe1c8a5965411a643b1fbd1fe
Daniel Barcos <dbarcosb@gmail.com>
2018-04-28 11:16:02 +0200

    feat: simplify & clean SW file

--- app/sw.js
+++ app/sw.js
@@ -1,11 +1,10 @@
 // defines what I want to cache when installing SW
-const staticCache = 'rw-static-v2';
+const staticCache = 'rw-static';
 const responsesCache = 'rw-responses';
 const urlsToStaticCache = [
     '/',
     '/index.html',
     '/restaurant.html',
-    // '/data/restaurants.json',
     '/css/main.css',
     '/js/dbhelper.js',
     '/js/main.js',
@@ -21,13 +20,12 @@ const urlsToStaticCache = [
     'img/9.jpg',
     'img/10.jpg'
 ];
-const contentImgsCache = 'rw-imgs-v1';
-const allCaches = [staticCache, contentImgsCache, responsesCache]; //here we keep the important caches we need
+
 
 // install event
 self.addEventListener('install', event => {
 
-    // e.waitUntil delays the event until the promise resolves
+    // delays the event until the promise resolves
     event.waitUntil(
 
         // Open the cache
@@ -40,86 +38,51 @@ self.addEventListener('install', event => {
     );
 });
 
-// activate event
-self.addEventListener('activate', event => {
-    console.log('SW activation');
 
-    event.waitUntil(
+// // activate event
+// const contentImgsCache = 'rw-imgs-v1';
+// const allCaches = [staticCache, contentImgsCache, responsesCache]; //here we keep the important caches we need
+// self.addEventListener('activate', event => {
+//     console.log('SW activation');
+//     event.waitUntil(
+//         // get all the cache keys
+//         caches.keys().then(cacheNames => {
+//             return Promise.all(
+//                 // filter all caches to find caches starting with rw- that are not inside allCaches and delete them
+//                 cacheNames.filter(cacheName => {
+//                     return cacheName.startsWith('rw-') && !allCaches.includes(cacheName);
+//                 }).map(cacheName => {
+//                     return caches.delete(cacheName);
+//                 })
+//             );
+//         })
+//     );
+// });
 
-        // get all the cache keys
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-
-                // filter all caches to find caches starting with rw- that are not inside allCaches and delete them
-                cacheNames.filter(cacheName => {
-                    return cacheName.startsWith('rw-') && !allCaches.includes(cacheName);
-                }).map(cacheName => {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-});
 
 // fetch event
 self.addEventListener('fetch', event => {
-    // const requestUrl = new URL(e.request.url);
-    // console.log('SW Fetch', requestUrl);
 
+    // delays the event until the promise resolves
     event.respondWith(
 
         // Check in cache for the request
         caches.match(event.request).then(response => {
 
             // If response in cache, return cached version
-            if (response) {
-                console.log('Served from cache:', event.request.url, response);
-                return response;
-            }
-
             // If response not in cache, fetch & cache
-            fetch(event.request).then(response => {
+            return response || fetch(event.request).then(response => {
 
                 // Open the cache
                 caches.open(responsesCache).then(cache => {
 
-                    // Put the cloned response in the cache
+                    // Put the request/response(cloned) in the cache
                     cache.put(event.request, response.clone());
-                    console.log('Response cached:', event.request.url);
 
+                    // return response
                     return response;
-                }); 
+                });
             });
-
         })
     );
 });
-
-
-// const requestUrl = new URL(e.request.url);
-// console.log(requestUrl.indexOf('https://maps.googleapi.com/js'));
-// if (requestUrl.origin === location.origin) {
-// console.log(requestUrl);
-// if (requestUrl.pathname.startsWith('/img/')) {
-//     e.respondWith(servePhoto(e.request));
-//     return;
-// }
-// e.respondWith(
-//     caches.match(e.request).then(response => {
-//         return response || fetch(e.request);
-//     })
-// );
-// }
-// function servePhoto(request) {
-//     const storageUrl = request.url.split('_')[0];
-//     return caches.open(contentImgsCache).then(cache => {
-//         return cache.match(storageUrl).then(response => {
-//             if (response) return response;
-
-//             return fetch(request).then(networkResponse => {
-//                 cache.put(storageUrl, networkResponse.clone());
-//                 return networkResponse;
-//             });
-//         });
-//     });
-// }
