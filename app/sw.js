ec8da16004a605de799b5f16f149219b87a5557a
Daniel Barcos <dbarcosb@gmail.com>
2018-05-31 09:15:23 +0200

    bug: Correct sw.js following review tips

--- app/sw.js
+++ app/sw.js
@@ -3,8 +3,19 @@ const staticCache = 'rw-static';
 const responsesCache = 'rw-responses';
 const urlsToStaticCache = [
     '/',
+    '/manifest.json',
     '/index.html',
     '/restaurant.html',
+    '/restaurant.html?id=1',
+    '/restaurant.html?id=2',
+    '/restaurant.html?id=3',
+    '/restaurant.html?id=4',
+    '/restaurant.html?id=5',
+    '/restaurant.html?id=6',
+    '/restaurant.html?id=7',
+    '/restaurant.html?id=8',
+    '/restaurant.html?id=9',
+    '/restaurant.html?id=10',
     '/css/main.css',
     '/js/dbhelper.js',
     '/js/main.js',
@@ -18,6 +29,15 @@ const urlsToStaticCache = [
     'img/7_sqip.svg',
     'img/8_sqip.svg',
     'img/9_sqip.svg',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
     'img/icons/fav-icon-o.png',
     'img/icons/fav-icon.png'
 ];
@@ -40,25 +60,25 @@ self.addEventListener('install', event => {
 });
 
 
-// // activate event
+// activate event
 // const contentImgsCache = 'rw-imgs-v1';
-// const allCaches = [staticCache, contentImgsCache, responsesCache]; //here we keep the important caches we need
-// self.addEventListener('activate', event => {
-//     console.log('SW activation');
-//     event.waitUntil(
-//         // get all the cache keys
-//         caches.keys().then(cacheNames => {
-//             return Promise.all(
-//                 // filter all caches to find caches starting with rw- that are not inside allCaches and delete them
-//                 cacheNames.filter(cacheName => {
-//                     return cacheName.startsWith('rw-') && !allCaches.includes(cacheName);
-//                 }).map(cacheName => {
-//                     return caches.delete(cacheName);
-//                 })
-//             );
-//         })
-//     );
-// });
+const allCaches = [staticCache, responsesCache]; //here we keep the important caches we need
+self.addEventListener('activate', event => {
+    console.log('SW activation');
+    event.waitUntil(
+        // get all the cache keys
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                // filter all caches to find caches starting with rw- that are not inside allCaches and delete them
+                cacheNames.filter(cacheName => {
+                    return cacheName.startsWith('rw-') && !allCaches.includes(cacheName);
+                }).map(cacheName => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
 
 
 // fetch event
